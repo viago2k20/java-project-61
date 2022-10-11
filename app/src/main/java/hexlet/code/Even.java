fc34657253feb5003d1eaccc6416e34be0f9a8c9
Denis <denzh@mail.ru>
2022-10-11 15:31:17 +0300

    add method printAnswer to Even.java

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -3,25 +3,26 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Even {
+    public static void printAnswer(String name, String no, String yes) {
+        System.out.println(no + " is wrong answer ;(. Correct answer was " + yes + ".\n"
+                + "Let's try again, " + name + "!");
+    }
+
     public static void playEven(String name) {
         int count = 0;
         String answer;
-
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         Scanner sc = new Scanner(System.in);
         int num;
         for (;;) {
-            num = (int) (Math.random() * (100 + 1));//from 0 to 100
+            num = (int) (Math.random() * (100 + 1)); //from 0 to 100
             System.out.println("Question: " + num);
             answer = sc.next();
-
             if ((num % 2 == 0 && answer.equals("no"))) {
-                System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'.\n"
-                        + "Let's try again, " + name + "!");
+                printAnswer(name, "no", "yes");
                 break;
             } else if ((num % 2 != 0 && answer.equals("yes"))) {
-                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.\n"
-                        + "Let's try again, " + name + "!");
+                printAnswer(name, "yes", "no");
                 break;
             } else {
                 System.out.println("Correct!");
