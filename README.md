799b10c7ad9a05c3e590aef2d4d2c228426921d4
Denis <denzh@mail.ru>
2022-10-11 13:16:09 +0300

    add changes on step 5

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/viago2k20/java-project-61/workflows/hexlet-check/badge.svg)](https://github.com/viago2k20/java-project-61/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/8a0e6722ec3bcaee4da7/maintainability)](https://codeclimate.com/github/viago2k20/java-project-61/maintainability)
+
+#game launch example
+https://asciinema.org/a/929bs297Kcue8fTY55JPP4LcG
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,46 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
+    public static String name;
+
+    public static void welcome(Scanner scanner) {
+        System.out.print("May I have your name? ");
+        scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.welcome();
+        Scanner sc = new Scanner(System.in);
+        String input;
+        boolean inGame = true;
+        //while (inGame) {
+        printMenu();
+        input = sc.nextLine();
+        switch (input) {
+            case "1" -> welcome(sc);
+            case "2" -> {
+                System.out.println("Playing Even");
+                welcome(sc);
+                Even.playEven(name);
+            }
+            case "0" -> {
+                System.out.println("Bye!");
+                //inGame = false;
+                System.out.print("\033[H\033[J"); //очистка экрана
+            }
+            default -> System.err.println("Wrong number!");
+        }
+        //}
+    }
+
+    public static void printMenu() {
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1. Greet\s
+                2. Even\s
+                0. Exit""");
     }
 }
