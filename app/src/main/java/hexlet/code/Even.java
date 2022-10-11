db9a183b4d42c23da42cf14c2644cd79a9516e3f
Denis <denzh@mail.ru>
2022-10-11 16:36:38 +0300

    changed code at Even.java

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -3,34 +3,32 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Even {
-    public static void printAnswer(String name, String no, String yes) {
-        System.out.println(no + " is wrong answer ;(. Correct answer was " + yes + ".\n"
-                + "Let's try again, " + name + "!");
-    }
 
     public static void playEven(String name) {
-        int count = 0;
         String answer;
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         Scanner sc = new Scanner(System.in);
         int num;
-        for (;;) {
+        for (int i = 0; i < 3; i++) {
             num = (int) (Math.random() * (100 + 1)); //from 0 to 100
             System.out.println("Question: " + num);
             answer = sc.next();
-            if ((num % 2 == 0 && answer.equals("no"))) {
-                printAnswer(name, "no", "yes");
-                break;
-            } else if ((num % 2 != 0 && answer.equals("yes"))) {
-                printAnswer(name, "yes", "no");
-                break;
+            String trueAnswer;
+            if (num % 2 == 0) {
+                trueAnswer = "yes";
             } else {
+                trueAnswer = "no";
+            }
+            if (answer.equals(trueAnswer)) {
                 System.out.println("Correct!");
-                count++;
-                if (count == 3) {
+                if (i == 2) {
                     System.out.println("Congratulations, " + name);
                     break;
                 }
+            } else {
+                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + trueAnswer + "'.\n"
+                        + "Let's try again, " + name + "!");
+                break;
             }
         }
     }
